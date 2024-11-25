c1e01edf2ad2c963e000ec61f70b09f5810f91e1
Alexander Danilov <a.v.danilov@volga.rt.ru>
2024-11-25 16:45:42 +0300

    lab1.1 add

--- lab1.1/src/main/java/ru/danilov/Main.java
+++ lab1.1/src/main/java/ru/danilov/Main.java
@@ -1,7 +1,20 @@
 package ru.danilov;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Person не создан так как контекст еще не создан
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        System.out.println("Number of beans: " + context.containsBean("person")); //context.getBeanDefinitionCount()
+        Person person1 = context.getBean(Person.class);
+        System.out.println(String.format("I`m one person: %s", person1));
+        Person person2 = context.getBean(Person.class);
+        if(person1 == person2)
+            System.out.println("I`m same person, person is SINGLETON");
+        else
+            System.out.println("I`m different person, person is PROTOTYPE");
     }
 }
--- lab1.1/src/main/java/ru/danilov/Person.java
+++ lab1.1/src/main/java/ru/danilov/Person.java
@@ -1,4 +1,27 @@
 package ru.danilov;
 
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Person {
+    private int age;
+    private String name;
+    private static int count = 0;
+
+    public Person() {
+        count++;
+        System.out.println("Person created, count = " + count);
+    }
+
+    //add toString
+    @Override
+    public String toString() {
+        return "Person{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
